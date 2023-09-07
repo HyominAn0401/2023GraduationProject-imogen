48a075c8917dbe4ecc61d8814d59f2b1e38e5efa
Hyomin-Ahn <98948416+Hyomin-Ahn@users.noreply.github.com>
2023-09-07 00:04:50 -0700

    Delete StyleTransfer1 directory

--- StyleTransfer1/styletransferapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StyletransferappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'styletransferapp'
--- StyleTransfer1/styletransferapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-# app_name = 'styletransferapp'
-
-urlpatterns = [
-    path('', views.upload_image, name='upload_image'),
-]
